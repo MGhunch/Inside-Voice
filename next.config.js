a2e6b11caedfe3a266fb1182a6aebf8c0b539097
MGhunch <michael@hunch.co.nz>
2026-03-31 00:14:26 +1300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -35,7 +35,7 @@ const nextConfig = {
               "default-src 'self'",
               "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-              "font-src 'self' https://fonts.gstatic.com",
+              "font-src 'self' data: https://fonts.gstatic.com",
               "img-src 'self' data: blob:",
               "connect-src 'self'",
               "frame-ancestors 'none'",
